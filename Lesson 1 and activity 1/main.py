d348671ec16caa3591118842c67bde5c2e550454
Daanish <rajreddym100@gmail.com>
2026-09-12 14:44:52 +0530

    variable

---
+++ Lesson 1 and activity 1/main.py
@@ -0,0 +1,9 @@
+
+print ("Hello, world! \n")
+
+
+print("My name is daanish \n")
+
+
+
+print("The result of 3 + 5 is:" , 3 + 5)
---
+++ routine.py
@@ -0,0 +1,11 @@
+print("My daily routine")
+
+print("I wake up at 7am.")
+
+print("I go to school at 8am.")
+
+print("I play with my friends in the evening.")
+
+print("I study for 2 hours in the night.")
+
+print("Total study hours in a week:",2*7)
