6282e08fb76a12d7558c2f4d479e1443249aa475
Daanish <rajreddym100@gmail.com>
2026-09-16 17:53:27 +0530

    birthdays

---
+++ Lesson 2 Activity 3/birthday.py
@@ -0,0 +1,14 @@
+Amy=("Amy: 1 - 3 - 2014")
+print (Amy)
+
+John=("John: 26 - 10 - 2014")
+print (John)
+
+Annie=("Annie: 1 - 9 - 2014")
+print (Annie)
+
+Caleb=("Caleb: 29 - 2 - 2014")
+print (Caleb)
+
+Marcus=("Marcus: 5 - 11 - 2014")
+print (Marcus)
