de536a92a300803a16977e98fc75fc3ca8ef9809
Daanish <rajreddym100@gmail.com>
2026-09-19 14:15:23 +0530

    average

---
+++ Lesson 3 Activity 1/average.py
@@ -0,0 +1,15 @@
+tree1 = 98
+
+tree2 = 94
+
+tree3 = 45
+
+tree4 = 95
+
+tree5 = 11
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("the sum of all the 5 trees is:", sum)
+
+average=sum/5
+print("average of all the trees is:", average)
